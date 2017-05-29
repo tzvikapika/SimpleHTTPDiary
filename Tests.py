e4dd98ed87374b805a46c64f48e068d4c246558a
Tzvi <you@example.com>
2017-05-29 18:58:55 +0300

    Finished Update and Delete tests

--- Tests.py
+++ Tests.py
@@ -31,47 +31,56 @@ class SimpleHTTPDiary_Tests(unittest.TestCase):
     ########################## Tests for adding new diary records functionality ##########################
     def test_addRecord_missing_date(self):
         response = self.app.post("/action", data = dict({"title": "", "desc": ""}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <date> <title> <desc> expected" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_missing_title(self):
         response = self.app.post("/action", data=dict({"date": "", "desc": ""}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <date> <title> <desc> expected" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_missing_desc(self):
         response = self.app.post("/action", data=dict({"date": "", "title": ""}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <date> <title> <desc> expected" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_empty_title(self):
         response = self.app.post("/action", data = dict({"date": "1-1-1900", "title":"", "desc": "someDesc1"}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Invalid Input, <title> <desc> can not be empty" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_empty_desc(self):
         response = self.app.post("/action", data=dict({"date": "1-1-1900", "title": "someTitle1", "desc": ""}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Invalid Input, <title> <desc> can not be empty" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_invalid_date_format(self):
         response = self.app.post("/action", data = dict({"date": "!@$23&*jhsdJWEG", "title": "someTitle1", "desc": "someDesc1"}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("does not match format '%d-%m-%Y'" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_invalid_date_length(self):
         response = self.app.post("/action", data=dict({"date": "1-1-19999", "title": "someTitle1", "desc": "someDesc1"}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("unconverted data remains:" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_invalid_date_yearOutOfRange(self):
         response = self.app.post("/action", data=dict({"date": "1-1-1899", "title": "someTitle1", "desc": "someDesc1"}))
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("year=1899 is before 1900; the datetime strftime() methods require year >= 1900" in response.data)
         self.assertTrue(len(SimpleHTTPDiary.diary) == 0)
 
     def test_addRecord_newEntry_minDate(self):
         newRec = {"date":"01-01-1900", "title":"someTitle1", "desc":"someDesc1"}
         response = self.app.post("/action", data = newRec)
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Added new entry" in response.data)
         self.assertTrue(SimpleHTTPDiary.diary[0]["date"] == "01-01-1900" and
                         SimpleHTTPDiary.diary[0]["title"] == "someTitle1" and
@@ -81,6 +90,7 @@ class SimpleHTTPDiary_Tests(unittest.TestCase):
     def test_addRecord_newEntry_maxDate(self):
         newRec = {"date":"31-12-9999", "title":"someTitle1", "desc":"someDesc1"}
         response = self.app.post("/action", data=newRec)
+        print "{0}{1}{2}".format("'POST' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Added new entry" in response.data)
         self.assertTrue(SimpleHTTPDiary.diary[0]["date"] == "31-12-9999" and
                         SimpleHTTPDiary.diary[0]["title"] == "someTitle1" and
@@ -88,75 +98,90 @@ class SimpleHTTPDiary_Tests(unittest.TestCase):
         self.assertTrue(len(SimpleHTTPDiary.diary) == 1)
 
 
+
     ########################## Tests for displaying existing diary records functionality ##########################
     def test_displayRecords_no_args(self):
         requestData = {}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <title> <desc> or/and <start> <end> expected" in response.data)
 
     def test_displayRecords_empty_title(self):
         requestData = {"title": "", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Invalid Input, <title> <desc> can not be empty" in response.data)
 
     def test_displayRecords_empty_desc(self):
         requestData = {"title": "someTitle1", "desc": ""}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Invalid Input, <title> <desc> can not be empty" in response.data)
 
     def test_displayRecords_missing_title(self):
         requestData = {"desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <title> <desc> expected" in response.data)
 
     def test_displayRecords_missing_desc(self):
         requestData = {"title": "someTitle1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <title> <desc> expected" in response.data)
 
     def test_displayRecords_missing_start(self):
         requestData = {"end": ""}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <start> <end> expected" in response.data)
 
     def test_displayRecords_missing_end(self):
         requestData = {"start": ""}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Insufficient Input, <start> <end> expected" in response.data)
 
     def test_displayRecords_invalid_start_date_format(self):
         requestData = {"start": "!@$23&*jhsdJWEG", "end":"31-12-9999", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("does not match format '%d-%m-%Y'" in response.data)
 
     def test_displayRecords_invalid_start_date_length(self):
         requestData = {"start": "31-12-99999", "end":"31-12-9999", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("unconverted data remains:" in response.data)
 
     def test_displayRecords_invalid_end_date_format(self):
         requestData = {"start": "31-12-9999", "end": "!@$23&*jhsdJWEG", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("does not match format '%d-%m-%Y'" in response.data)
 
     def test_displayRecords_invalid_end_date_length(self):
         requestData = {"start": "31-12-9999", "end": "31-12-99999", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("unconverted data remains:" in response.data)
 
     def test_displayRecords_date_start_greater_than_end(self):
         requestData = {"start": "31-12-9999", "end": "1-1-1900", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("Start date can not be greater than End date" in response.data)
 
     def test_displayRecords_invalid_start_date_yearOutOfRange(self):
         requestData = {"start": "31-12-1899", "end": "1-1-1900", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("year=1899 is before 1900; the datetime strftime() methods require year >= 1900" in response.data)
 
     def test_displayRecords_invalid_end_date_yearOutOfRange(self):
         requestData = {"start": "31-12-1900", "end": "1-1-1899", "title": "someTitle1", "desc": "someDesc1"}
         response = self.app.get("/action", data=requestData)
+        print "{0}{1}{2}".format("'GET' RESULT:\n", response.data, json.dumps(SimpleHTTPDiary.diary, indent=4))
         self.assertTrue("year=1899 is before 1900; the datetime strftime() methods require year >= 1900" in response.data)
     # All the above 'displayRecords' tests check common functionality to GET/PUT/DELETE which is implemented within SimpleHTTPDiary.Search() func #
 
@@ -185,19 +210,118 @@ class SimpleHTTPDiary_Tests(unittest.TestCase):
         self.assertTrue(len(responseDecoded) == 1)
 
     ########################## Tests for updating existing diary records functionality ##########################
-    def test_updateRecords_by_title_desc(self):
-        pass
+    def test_updateRecords_by_title_desc_newTitle(self):
+        self.initAppData()
+        requestData = {"title": "big", "desc": "Desc", "newTitle": "NewTestTitle"}
+        response = self.app.put("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'PUT' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 3)
+        for rec in responseDecoded:
+            diaryIndex = SimpleHTTPDiary.diary.index(rec)
+            if SimpleHTTPDiary.diary[diaryIndex]['title'] != "NewTestTitle":
+                self.fail("Failed to update title")
+
+    def test_updateRecords_by_title_desc_newDesc(self):
+        self.initAppData()
+        requestData = {"title": "big", "desc": "Desc", "newDesc": "NewTestDesc"}
+        response = self.app.put("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'PUT' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 3)
+        for rec in responseDecoded:
+            diaryIndex = SimpleHTTPDiary.diary.index(rec)
+            if SimpleHTTPDiary.diary[diaryIndex]['desc'] != "NewTestDesc":
+                self.fail("Failed to update desc")
+
+    def test_updateRecords_by_title_desc_newTitle_newDesc(self):
+        self.initAppData()
+        requestData = {"title": "big", "desc": "Desc", "newTitle": "NewTestTitle", "newDesc": "NewTestDesc"}
+        response = self.app.put("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'PUT' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 3)
+        for rec in responseDecoded:
+            diaryIndex = SimpleHTTPDiary.diary.index(rec)
+            if SimpleHTTPDiary.diary[diaryIndex]['title'] != "NewTestTitle" and SimpleHTTPDiary.diary[diaryIndex]['desc'] != "NewTestDesc":
+                self.fail("Failed to update title/desc")
+
     def test_updateRecords_by_start_end(self):
-        pass
+        self.initAppData()
+        requestData = {"start": "1-1-1900", "end": "31-12-9999", "newTitle": "NewTestTitle", "newDesc": "NewTestDesc"}
+        response = self.app.put("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'PUT' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 6)
+        for rec in responseDecoded:
+            diaryIndex = SimpleHTTPDiary.diary.index(rec)
+            if SimpleHTTPDiary.diary[diaryIndex]['title'] != "NewTestTitle" and SimpleHTTPDiary.diary[diaryIndex]['desc'] != "NewTestDesc":
+                self.fail("Failed to update title/desc")
+
     def test_updateRecords_by_title_desc_start_end(self):
-        pass
+        self.initAppData()
+        requestData = {"start": "1-3-2001", "end": "21-7-2002", "title": "nice", "desc": "some", "newTitle": "NewTestTitle", "newDesc": "NewTestDesc"}
+        response = self.app.put("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'PUT' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 1)
+        for rec in responseDecoded:
+            diaryIndex = SimpleHTTPDiary.diary.index(rec)
+            if SimpleHTTPDiary.diary[diaryIndex]['title'] != "NewTestTitle" and SimpleHTTPDiary.diary[diaryIndex]['desc'] != "NewTestDesc":
+                self.fail("Failed to update title/desc")
+
     ########################## Tests for deleting existing diary records functionality ##########################
     def test_deleteRecords_title_desc(self):
-        pass
+        self.initAppData()
+        requestData = {"title": "big", "desc": "Desc"}
+        response = self.app.delete("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'DELETE' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 3)
+        for rec in responseDecoded:
+            try:
+                diaryIndex = SimpleHTTPDiary.diary.index(rec)
+                self.fail("Record was not deteted")
+            except ValueError as ex:
+                print "{0}{1}".format("Record deleted: ", ex)
+
     def test_deleteRecords_start_end(self):
-        pass
+        self.initAppData()
+        requestData = {"start": "1-1-1900", "end": "31-12-9999"}
+        response = self.app.delete("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'DELETE' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 6)
+        for rec in responseDecoded:
+            try:
+                diaryIndex = SimpleHTTPDiary.diary.index(rec)
+                self.fail("Record was not deteted")
+            except ValueError as ex:
+                print "{0}{1}".format("Record deleted: ", ex)
+
+
     def test_deleteRecords_title_desc_start_end(self):
-        pass
+        self.initAppData()
+        requestData = {"start": "1-3-2001", "end": "21-7-2002", "title": "nice", "desc": "some"}
+        response = self.app.delete("/action", data=requestData)
+        responseDecoded = json.loads(response.data)
+        print "{0}{1}".format("'DELETE' RESULT:\n", json.dumps(responseDecoded, indent=4))
+
+        self.assertTrue(len(responseDecoded) == 1)
+        for rec in responseDecoded:
+            try:
+                diaryIndex = SimpleHTTPDiary.diary.index(rec)
+                self.fail("Record was not deteted")
+            except ValueError as ex:
+                print "{0}{1}".format("Record deleted: ", ex)
+
     ########################## Tests for saving diary backup ##########################
     def test_saveRecordsBackup_xyz(self):
         pass
